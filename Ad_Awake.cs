8f17fdcaa8a7e261272f98bc24e52534c38e9db8
Hossam-Exe <58437507+Hossam-Exe@users.noreply.github.com>
2022-10-01 15:43:30 +0200

    Add files via upload

---
+++ Ad_Awake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ad_Awake : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId= "4567342";
+    [SerializeField] string _iOsGameId;
+    [SerializeField] bool _testMode = true;
+   
+    private string _gameId;
+
+    void Awake()
+    {
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode);
+    }
+
+    public void OnInitializationComplete()
+    {
+      
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        
+    }
+}
